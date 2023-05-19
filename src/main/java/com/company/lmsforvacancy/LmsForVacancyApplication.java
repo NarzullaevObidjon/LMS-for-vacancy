56c7d6be7c19a45d29541ce3d39cb34d63fb414f
NarzullaevObidjon <obidsattarovich3600@gmail.com>
2023-05-19 12:27:24 +0500

    initial

---
+++ src/main/java/com/company/lmsforvacancy/LmsForVacancyApplication.java
@@ -0,0 +1,13 @@
+package com.company.lmsforvacancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsForVacancyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsForVacancyApplication.class, args);
+    }
+
+}
